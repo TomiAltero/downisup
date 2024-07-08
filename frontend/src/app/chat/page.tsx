19aed0b5f81f328a0b08950ec376f3e30a574497
TomiAltero <tomasaltero@gmail.com>
2024-07-08 01:16:25 -0300

    cambios

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import AppLayout from "@/app/applayout";
-import Chat from "@/components/chat";
 
 export default function Page() {
   return (
     <AppLayout>
       <section className="flex justify-center items-center h-full">
-        <h1 className="text-xl  text-center">
-          <Chat />
+        <h1 className="text-xl text-center text-gray-500">
+          Sistema de chat (proximamente)
         </h1>
       </section>
     </AppLayout>
