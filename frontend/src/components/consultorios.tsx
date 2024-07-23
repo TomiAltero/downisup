f98889e3628ade5ef6c768869c65b7ea59315723
FacundoBisio <facubisio433@gmail.com>
2024-07-23 08:37:46 -0300

    transiciones consultorios

--- frontend/src/components/consultorios.tsx
+++ frontend/src/components/consultorios.tsx
@@ -1,7 +1,29 @@
+"use client"
+import { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 import Image from 'next/image';
 import ubicacionImage from '../../public/ubicacion.png'; // Ajusta la ruta según sea necesario
+import { useInView } from 'react-intersection-observer';
 
 const Consultorios = () => {
+  const controls = useAnimation();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    }
+  }, [controls, inView]);
+
+  const textVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.5 }
+    }
+  };
+
   return (
     <section className="bg-white py-8">
       <div className="container mx-auto px-4">
@@ -16,10 +38,16 @@ const Consultorios = () => {
             height={400}
           />
         </div>
-        <div className="flex flex-col lg:flex-row lg:space-x-8">
+        <motion.div
+          className="flex flex-col lg:flex-row lg:space-x-8"
+          ref={ref}
+          initial="hidden"
+          animate={controls}
+          variants={textVariants}
+        >
           <div className="lg:w-1/2 mb-4 lg:mb-0">
-            <h3 className="text-xl font-semibold text-blue-900 mb-2">Nuestros profesionales</h3>
-            <ul className="list-disc list-inside text-base text-gray-700 space-y-1">
+            <h3 className="text-lg font-semibold text-blue-900 mb-2">Nuestros profesionales</h3>
+            <ul className="list-disc list-inside text-sm text-gray-700 space-y-1">
               <li>Cecilia Cedrola: Lic. en Psicopedagogía / Magister en atención a personas con síndrome de Down.</li>
               <li>Laura Legeren: Lic. en Psicología / Posgrado en Psicoterapia cognitivo conductual.</li>
               <li>Belén Ruiz: Lic. en Psicopedagogía - Especialización en estimulación temprana / Profesora en educación especial.</li>
@@ -29,11 +57,11 @@ const Consultorios = () => {
           </div>
           <div className="lg:w-1/2">
             <h3 className="text-xl font-semibold text-blue-900 mb-2">Programas de tratamiento</h3>
-            <p className="text-gray-700 text-base leading-relaxed">
+            <p className="text-gray-700 text-sm leading-relaxed">
               Los programas de tratamiento están diseñados para mejorar la calidad de vida de las personas con discapacidad y su participación activa en los ámbitos familiares, escolares, laborales y comunitarios. Nuestra meta y diferencial en los consultorios, es nuestro motor: a través de un trabajo grupal e integral de nuestros profesionales y la familia de cada paciente, lograr su autonomía; planteándonos objetivos específicos para cada uno, siendo la comunicación y la experiencia fundamentales para el correcto funcionamiento del tratamiento.
             </p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
