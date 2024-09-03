203c18657c3414856ba08868b5455f29bc32a246
FacundoBisio <facubisio433@gmail.com>
2024-09-03 11:48:35 -0300

    mapa weon

--- frontend/src/components/MapComponent.tsx
+++ frontend/src/components/MapComponent.tsx
@@ -20,7 +20,7 @@ const MapComponent: React.FC = () => {
   }, []);
 
   return (
-    <MapContainer className='rounded-3xl outline-none md:h-[500px] md:w-[700px] lg:h-[300px] lg:w-[1500px] z-10' center={position} zoom={15}>
+    <MapContainer className='rounded-3xl outline-none h-[300px] w-full z-10' center={position} zoom={15}>
       <TileLayer
         url={`https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmFjdWIiLCJhIjoiY20wZ3JqNGwxMDNycDJxcTJrczJ0ZzdhZyJ9.u9Cca9Ah-oFVlYlExqmbIQ`}
         attribution='&copy; <a href="https://www.mapbox.com/">Mapbox</a>'
