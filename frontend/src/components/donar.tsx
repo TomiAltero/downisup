900b1411bf0417bbde5f2d0e3169d596b7826742
Mateo <you@example.com>
2024-11-12 11:49:44 -0300

    arreglo error url donar

--- frontend/src/components/donar.tsx
+++ frontend/src/components/donar.tsx
@@ -33,58 +33,69 @@ const Donar = () => {
   
   return (
     <section className="bg-white">
-
-      <section className="relative w-full h-[250px] mb-10 lg:h-[500px]" style={{ height: "250px" }}> {/* Cambia el tamaño según la pantalla */}
+      <section className="relative w-full h-[250px] mb-10 lg:h-[500px]" style={{ height: "250px" }}>
         <div className="absolute inset-0 z-10 flex items-center justify-center">
-          <h1 className="text-white text-4xl font-bold mt-10">DONAR</h1> {/* Texto más grande en pantallas grandes */}
+          <h1 className="text-white text-4xl font-bold mt-10">DONAR</h1>
         </div>
 
-        <Swiper
-          modules={[Navigation, Autoplay]}
-          spaceBetween={30}
-          centeredSlides={true}
-          autoplay={{
-            delay: 3000,
-            disableOnInteraction: false,
-          }}
-          loop={true}
-          navigation={false}
-          allowTouchMove={false}
-          className="h-full w-full"
-        >
-          <SwiperSlide className="relative h-full">
-            <Image
-              src="/background.png"
-              alt="Background 1"
-              layout="fill"
-              objectFit="cover"
-              className="z-[-1]"
-            />
-            <div className="absolute inset-0 bg-gradient-to-b from-blue-700 to-blue-700 opacity-50"></div>
-          </SwiperSlide>
+        {/* Swiper component inside try-catch */}
+        <div>
+          {(() => {
+            try {
+              return (
+                <Swiper
+                  modules={[Navigation, Autoplay]}
+                  spaceBetween={30}
+                  centeredSlides={true}
+                  autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                  }}
+                  loop={true}
+                  navigation={false}
+                  allowTouchMove={false}
+                  className="h-full w-full"
+                >
+                  <SwiperSlide className="relative h-full">
+                    <Image
+                      src="/background.png"
+                      alt="Background 1"
+                      layout="fill"
+                      objectFit="cover"
+                      className="z-[-1]"
+                    />
+                    <div className="absolute inset-0 bg-gradient-to-b from-blue-700 to-blue-700 opacity-50"></div>
+                  </SwiperSlide>
 
-          <SwiperSlide className="relative h-full">
-            <Image
-              src="/chicasPlaza.jpg"
-              alt="Background 2"
-              layout="fill"
-              objectFit="cover"
-              className="z-[-1]"
-            />
-            <div className="absolute inset-0 bg-gradient-to-b from-blue-700 to-blue-700 opacity-50"></div>
-          </SwiperSlide>
+                  <SwiperSlide className="relative h-full">
+                    <Image
+                      src="/chicasPlaza.jpg"
+                      alt="Background 2"
+                      layout="fill"
+                      objectFit="cover"
+                      className="z-[-1]"
+                    />
+                    <div className="absolute inset-0 bg-gradient-to-b from-blue-700 to-blue-700 opacity-50"></div>
+                  </SwiperSlide>
 
-          <SwiperSlide className="relative h-full">
-            <Image
-              src="/chicos.jpeg"
-              alt="Background 3"
-              layout="fill"
-              objectFit="cover"
-              className="z-[-1]"
-            />
-            <div className="absolute inset-0 bg-gradient-to-b from-blue-700 to-blue-700 opacity-50"></div>
-          </SwiperSlide>
-        </Swiper>
+                  <SwiperSlide className="relative h-full">
+                    <Image
+                      src="/chicos.jpeg"
+                      alt="Background 3"
+                      layout="fill"
+                      objectFit="cover"
+                      className="z-[-1]"
+                    />
+                    <div className="absolute inset-0 bg-gradient-to-b from-blue-700 to-blue-700 opacity-50"></div>
+                  </SwiperSlide>
+                </Swiper>
+              );
+            } catch (error) {
+              console.error("Error en Swiper:", error);
+              return <p>Error al cargar el slider</p>;
+            }
+          })()}
+        </div>
       </section>
 
       <div className="container mx-auto px-4">
@@ -99,25 +110,25 @@ const Donar = () => {
       </div>
 
       <div className="flex flex-col items-center justify-center w-full">
-          <div className="w-full lg:w-8/12"> {/* Ajusta el ancho según la pantalla */}
-            <h1 className='text-custom-blue text-3xl lg:text-4xl text-center font-semibold mb-5'>
-              La solidaridad empieza en casa, ¿te sumás?
-            </h1>
-              <p className="text-gray-600 text-center px-4 text-base lg:text-lg">
-                En Downisup creemos firmemente que la solidaridad empieza en casa. Se aprende y se cultiva a través de la práctica de la entrega generosa, por eso te invitamos a acompañarnos en esta causa.
-              </p>
-          </div>
+        <div className="w-full lg:w-8/12">
+          <h1 className='text-custom-blue text-3xl lg:text-4xl text-center font-semibold mb-5'>
+            La solidaridad empieza en casa, ¿te sumás?
+          </h1>
+          <p className="text-gray-600 text-center px-4 text-base lg:text-lg">
+            En Downisup creemos firmemente que la solidaridad empieza en casa. Se aprende y se cultiva a través de la práctica de la entrega generosa, por eso te invitamos a acompañarnos en esta causa.
+          </p>
+        </div>
 
-          <div className="flex justify-center mt-5">          
-            <Image
-              src={solaridad}
-              alt="solaridad"
-              className="items-center justify-center rounded-3xl h-40 w-40 lg:h-72 lg:w-72"
-              layout="responsive"
-            />
-          </div>
+        <div className="flex justify-center mt-5">          
+          <Image
+            src={solaridad}
+            alt="solaridad"
+            className="items-center justify-center rounded-3xl h-40 w-40 lg:h-72 lg:w-72"
+            layout="responsive"
+          />
+        </div>
 
-        <div className="w-full lg:w-8/12 mt-5 text-center px-5"> {/* Ajustado para diferentes pantallas */}
+        <div className="w-full lg:w-8/12 mt-5 text-center px-5">
           <h1 className="text-custom-blue text-2xl lg:text-4xl font-semibold mb-5">
             ¿Cómo podés ayudar?
           </h1>
@@ -136,7 +147,7 @@ const Donar = () => {
 
         <div className="w-full lg:w-8/12 text-center px-4">
           <p className="text-gray-600 px-10 text-sm lg:text-base mb-2">
-          También, ya sea que realices una donación mensual o una donación única, te brindamos estos medios de contribución directamente en nuestro sitio web. Podés donar con tarjeta de crédito, débito bancario o mediante Pago Fácil y Rapipago. Para administrar tu donación a través de nuestro sitio web, seleccioná la frecuencia que prefieras y seguí todos los pasos indicados:
+            También, ya sea que realices una donación mensual o una donación única, te brindamos estos medios de contribución directamente en nuestro sitio web. Podés donar con tarjeta de crédito, débito bancario o mediante Pago Fácil y Rapipago. Para administrar tu donación a través de nuestro sitio web, seleccioná la frecuencia que prefieras y seguí todos los pasos indicados:
           </p>
         </div>
 
--- frontend/src/components/tienda.tsx
+++ frontend/src/components/tienda.tsx
@@ -1,3 +1,4 @@
+// components/tienda.tsx
 'use client'
 import React, { useState } from 'react';
 import Image from "next/image";
@@ -5,10 +6,9 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import LandingLayout from "@/layouts/LandingLayout";
 import { FooterWithLinks } from "@/components/footerLinks";
-import { ProductCard } from "./ProductCard";  // Asegúrate de que la ruta esté correcta
-import { FaTrash } from 'react-icons/fa'; // Importa el icono de basura
+import { ProductCard } from "./ProductCard";  
+import { FaTrash } from 'react-icons/fa';
 
 const products = [
   { 
@@ -25,7 +25,7 @@ const products = [
     image: "/medias.png", 
     hoverImage: "/mision.png",  
   },
-  // Agrega más productos aquí con sus respectivas imágenes
+  // Agrega más productos aquí
 ];
 
 export default function Tienda() {
@@ -44,7 +44,6 @@ export default function Tienda() {
   const addToCart = (product: any) => {
     setCart((prevCart) => {
       const existingProduct = prevCart.find(item => item.id === product.id);
-
       if (existingProduct) {
         return prevCart.map(item => 
           item.id === product.id 
@@ -58,11 +57,9 @@ export default function Tienda() {
     setTotal((prevTotal) => prevTotal + parseFloat(product.price));
   };
 
-  
   const removeFromCart = (productId: number, price: string) => {
     setCart((prevCart) => {
       const existingProduct = prevCart.find(item => item.id === productId);
-
       if (existingProduct && existingProduct.quantity > 1) {
         return prevCart.map(item => 
           item.id === productId 
@@ -77,8 +74,8 @@ export default function Tienda() {
   };
 
   return (
-    <LandingLayout>
-      {/* Sección del carrusel de productos destacados */}
+    <section>
+      {/* Carrusel de productos destacados */}
       <section className="relative w-fullscreen pt-16" style={{ height: "250px" }}>
         <div className="absolute inset-0 z-10 flex items-center justify-center">
           <h1 className="text-white text-4xl font-bold mt-10">NUESTRA TIENDA</h1>
@@ -112,61 +109,65 @@ export default function Tienda() {
         </Swiper>
       </section>
 
-      <div className="flex justify-center">
-        <main className="p-4 w-full max-w-screen-xl mx-auto">
-          <section className="gap-10 py-10">
-            <h1 className="text-3xl text-custom-blue font-bold text-center mb-8">Productos destacados</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 lg:gap-10">
-              {products.map((product) => (
-                <ProductCard
-                  key={product.id}
-                  id={product.id}
-                  name={product.name}
-                  price={product.price}
-                  image={product.image}
-                  hoverImage={product.hoverImage}
-                  hovered={hoveredProductId === product.id}
-                  onHover={handleMouseEnter}
-                  onLeave={handleMouseLeave}
-                  onAddToCart={() => addToCart(product)}
-                />
-              ))}
-            </div>
-          </section>
+      <main className="p-4 w-full max-w-screen-xl mx-auto">
+        {/* Productos destacados */}
+        <section className="gap-10 py-10">
+          <h1 className="text-3xl text-custom-blue font-bold text-center mb-8">Productos destacados</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 lg:gap-10">
+            {products.map((product) => (
+              <ProductCard
+                key={product.id}
+                id={product.id}
+                name={product.name}
+                price={product.price}
+                image={product.image}
+                hoverImage={product.hoverImage}
+                hovered={hoveredProductId === product.id}
+                onHover={handleMouseEnter}
+                onLeave={handleMouseLeave}
+                onAddToCart={() => addToCart(product)}
+              />
+            ))}
+          </div>
+        </section>
 
-          <section className="py-10">
-            <h2 className="text-2xl font-bold mb-4 text-custom-blue">Carrito de compras</h2>
-            {cart.length === 0 ? (
-              <p>Tu carrito está vacío</p>
-            ) : (
-              <>
-                <ul>
-                  {cart.map((item) => (
-                    <li key={item.id} className="flex justify-between text-lg items-center mb-4">
-                      <span>{item.name} - ${item.price} x {item.quantity}</span>
-                      <button 
-                        className="bg-red text-white px-2 py-1 rounded-lg flex items-center" 
-                        onClick={() => removeFromCart(item.id, item.price)}
-                      >
-                        <FaTrash className="mr-2" /> {/* Icono de basura */}
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-                <div className="text-right text-xl text-custom-blue font-bold">
-                  Total: ${total.toFixed(2)}
-                </div>
-                <button className="bg-black">
-                  <a className="bg-custom-blue text-white px-4 py-2 rounded-lg mt-4 w-full text-center hover:bg-blue-700 transition duration-300" 
-                    href="/application/form-checkout">Finalizar Compra</a>
-                </button>
-              </>
-            )}
-          </section>
-        </main>
-      </div>
+        {/* Carrito de compras */}
+        <section className="py-10">
+          <h2 className="text-2xl font-bold mb-4 text-custom-blue">Carrito de compras</h2>
+          {cart.length === 0 ? (
+            <p>Tu carrito está vacío</p>
+          ) : (
+            <>
+              <ul>
+                {cart.map((item) => (
+                  <li key={item.id} className="flex justify-between text-lg items-center mb-4">
+                    <span>{item.name} - ${item.price} x {item.quantity}</span>
+                    <button 
+                      className="bg-red text-white px-2 py-1 rounded-lg flex items-center" 
+                      onClick={() => removeFromCart(item.id, item.price)}
+                    >
+                      <FaTrash className="mr-2" /> {/* Icono de basura */}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+              <div className="text-right text-xl text-custom-blue font-bold">
+                Total: ${total.toFixed(2)}
+              </div>
+              <button className="bg-black">
+                <a 
+                  className="bg-custom-blue text-white px-4 py-2 rounded-lg mt-4 w-full text-center hover:bg-blue-700 transition duration-300" 
+                  href="/application/form-checkout"
+                >
+                  Finalizar Compra
+                </a>
+              </button>
+            </>
+          )}
+        </section>
+      </main>
 
       <FooterWithLinks />
-    </LandingLayout>
+    </section>
   );
 }
