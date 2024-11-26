5a0242a248c61f761e3f6a7eb9c56f893381e07e
TomiAltero <tomasaltero@gmail.com>
2024-11-25 23:24:51 -0300

    cambios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,20 @@ import { ButtonHome, ButtonShowMore } from "@/components/Button";
 import Link from "next/link";
 import Image from "next/image";
 import LandingLayout from "@/layouts/LandingLayout";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Autoplay } from 'swiper/modules'; // Importamos Autoplay para el deslizamiento automático
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/autoplay'; // Importamos CSS de autoplay
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules"; // Importamos Autoplay para el deslizamiento automático
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/autoplay"; // Importamos CSS de autoplay
 import { Footer } from "../components/footer";
 import { FooterWithLinks } from "@/components/footerLinks";
-import { PhoneIconSvg, MessageIconSvg, PinIconSvg, CalendarIcon} from "@/components/ui/icons"
+import {
+  PhoneIconSvg,
+  MessageIconSvg,
+  PinIconSvg,
+  CalendarIcon,
+} from "@/components/ui/icons";
+import "./globals.css";
 
 // CSS para las animaciones
 const styles = `
@@ -35,18 +41,16 @@ const eventCards = [
     shortDescription: "Todos somos iguales, pero diferentes",
     longDescription: "Long description for Event 1",
     image: "/DiaInternacional.webp",
-    date : "Martes 3 de Septiembre"
+    date: "Martes 3 de Septiembre",
   },
   {
     title: "2das Jornadas sobre el Síndrome de Down",
     shortDescription: "Educar hijos en el camino a la autonomia.",
     longDescription: "Long description for Event 2",
     image: "/Jornadas.jpg",
-    date : "Martes 3 de Septiembre"
-
+    date: "Martes 3 de Septiembre",
   },
 ];
-
 
 const handleScrollAnimations = () => {
   const observer = new IntersectionObserver(
@@ -57,7 +61,7 @@ const handleScrollAnimations = () => {
         }
       });
     },
-    { threshold: 0.1 }
+    { threshold: 0.1 },
   );
 
   document.querySelectorAll(".fade-in").forEach((element) => {
@@ -124,12 +128,18 @@ export default function Home() {
                 Bienvenidos a
               </h1>
               <article className="text-center text-white">
-                <h1 className="text-6xl mb-4 text-white font-raleway">DownIsUp</h1>
-                <h1 className="text-6xl text-white font-raleway mb-10">Córdoba</h1>
+                <h1 className="text-6xl mb-4 text-white font-raleway">
+                  DownIsUp
+                </h1>
+                <h1 className="text-6xl text-white font-raleway mb-10">
+                  Córdoba
+                </h1>
               </article>
             </section>
             <section className="flex flex-col items-center z-10">
-                <div className="bg-custom-blue shadow-lg shadow-indigo-500/50 rounded-lg"><ButtonHome /></div>
+              <div className="bg-custom-blue shadow-lg shadow-indigo-500/50 rounded-lg">
+                <ButtonHome />
+              </div>
             </section>
           </section>
 
@@ -143,29 +153,75 @@ export default function Home() {
                 height={100}
                 className="lg:-mt-30 md:mt-0  mb-4 md:mb-6"
               />
-              <p className="text-white text-lg md:text-xl leading-relaxed">&quot;LOGRAR LA</p>
-              <p className="text-white text-xl md:text-2xl font-extrabold leading-relaxed">AUTONOMÍA Y VIDA INDEPENDIENTE</p>
-              <p className="text-white text-lg md:text-xl leading-relaxed">ES UN DESAFÍO Y UN RETO, TANTO PARA LA PERSONA, COMO PARA SU ENTORNO&quot;</p>
+              <p className="text-white text-lg md:text-xl leading-relaxed">
+                &quot;LOGRAR LA
+              </p>
+              <p className="text-white text-xl md:text-2xl font-extrabold leading-relaxed">
+                AUTONOMÍA Y VIDA INDEPENDIENTE
+              </p>
+              <p className="text-white text-lg md:text-xl leading-relaxed">
+                ES UN DESAFÍO Y UN RETO, TANTO PARA LA PERSONA, COMO PARA SU
+                ENTORNO&quot;
+              </p>
             </div>
 
             {/* División diagonal, solo visible en pantallas medianas y más grandes */}
-            <div className="hidden md:block absolute w-full h-full -inset-x-0 z-0 bg-custom-blue" style={{ clipPath: "polygon(0 0, 38% 0, 25% 100%, 0% 100%)" }}></div>
+            <div
+              className="hidden md:block absolute w-full h-full -inset-x-0 z-0 bg-custom-blue"
+              style={{ clipPath: "polygon(0 0, 38% 0, 25% 100%, 0% 100%)" }}
+            ></div>
 
             <div className="w-full md:w-8/12 flex flex-col justify-center items-center p-6 md:p-10 z-10">
-              <h2 className="lg:text-4xl md:text-5xl font-bold text-custom-blue mt-5 mb-5 md:mb-6">NUESTRA HISTORIA</h2>
+              <h2 className="lg:text-4xl md:text-5xl font-bold text-custom-blue mt-5 mb-5 md:mb-6">
+                NUESTRA HISTORIA
+              </h2>
               <p className="text-base md:text-lg text-custom-blue mb-6 md:mb-10">
-                Comenzó en 2015 cuando decidimos comenzar a organizar charlas, acciones y más, trabajando para lograr el cambio que buscábamos. Nos basamos en la existencia de un movimiento nacional llamado Down is Up, con sede en varias provincias, con el fin de satisfacer la necesidad de contención, atención e información sobre el Síndrome de Down.
+                Comenzó en 2015 cuando decidimos comenzar a organizar charlas,
+                acciones y más, trabajando para lograr el cambio que buscábamos.
+                Nos basamos en la existencia de un movimiento nacional llamado
+                Down is Up, con sede en varias provincias, con el fin de
+                satisfacer la necesidad de contención, atención e información
+                sobre el Síndrome de Down.
               </p>
               <p className="text-custom-blue text-base md:text-lg mb-6">
-                <Image src="/tick.png" alt="Tick" className="inline-block mr-2" width={16} height={16} /> Promover los derechos reconocidos en la Convención Internacional sobre los Derechos de las Personas con Discapacidad.<br />
-                <Image src="/tick.png" alt="Tick" className="inline-block mr-2" width={16} height={16} /> Visibilizar el Síndrome de Down, mediante acciones de educación, formación y actividades sociales y culturales, fortaleciendo la convivencia.<br />
-                <Image src="/tick.png" alt="Tick" className="inline-block mr-2" width={16} height={16} /> Fomentar la vida independiente y la autonomía de las personas con síndrome de Down.
+                <Image
+                  src="/tick.png"
+                  alt="Tick"
+                  className="inline-block mr-2"
+                  width={16}
+                  height={16}
+                />{" "}
+                Promover los derechos reconocidos en la Convención Internacional
+                sobre los Derechos de las Personas con Discapacidad.
+                <br />
+                <Image
+                  src="/tick.png"
+                  alt="Tick"
+                  className="inline-block mr-2"
+                  width={16}
+                  height={16}
+                />{" "}
+                Visibilizar el Síndrome de Down, mediante acciones de educación,
+                formación y actividades sociales y culturales, fortaleciendo la
+                convivencia.
+                <br />
+                <Image
+                  src="/tick.png"
+                  alt="Tick"
+                  className="inline-block mr-2"
+                  width={16}
+                  height={16}
+                />{" "}
+                Fomentar la vida independiente y la autonomía de las personas
+                con síndrome de Down.
               </p>
-              <div className="bg-pink-600 shadow-lg shadow-pink-500/50 rounded-lg "><ButtonShowMore /></div>
+              <div className="bg-pink-600 shadow-lg shadow-pink-500/50 rounded-lg ">
+                <ButtonShowMore />
+              </div>
             </div>
           </section>
 
-          <Footer/>
+          <Footer />
 
           {/* Tercera sección */}
           <section className="min-h-[calc(100vh-120px)] w-full flex flex-col md:flex-row snap-start transition-transform duration-1200 ease-in-out relative fade-in">
@@ -178,13 +234,21 @@ export default function Home() {
                 height={1400}
                 className="mb-4 md:mb-6 rounded-3xl"
               />
-              <a href="https://maps.app.goo.gl/cpvhnUq2XH37BnCi8" className="text-custom-blue text-lg mt-5 mb-5 leading-relaxed flex items-center hover:underline"><PinIconSvg/>Pérez de Herrera 2053</a>
+              <a
+                href="https://maps.app.goo.gl/cpvhnUq2XH37BnCi8"
+                className="text-custom-blue text-lg mt-5 mb-5 leading-relaxed flex items-center hover:underline"
+              >
+                <PinIconSvg />
+                Pérez de Herrera 2053
+              </a>
             </div>
 
             {/* División diagonal invertida */}
             <div
               className="hidden md:block absolute w-full h-full -inset-x-0 z-0 bg-custom-blue"
-              style={{ clipPath: "polygon(50% 0, 100% 0, 100% 100%, 60% 100%)" }}
+              style={{
+                clipPath: "polygon(50% 0, 100% 0, 100% 100%, 60% 100%)",
+              }}
             ></div>
 
             {/* Sección con imagen y texto a la derecha */}
@@ -193,42 +257,71 @@ export default function Home() {
                 VENI A CONOCERNOS
               </h2>
               <p className="text-white text-base lg:mx-30 md:mx-10 leading-relaxed">
-                Programas de tratamiento diseñados para mejorar la calidad  de vida y la participación de personas con discapacidad en diversos ámbitos. En los consultorios, se enfocan en trabajar 
-                de manera integral con profesionales y familias para lograr la autonomía de cada paciente, estableciendo objetivos específicos y enfatizando la comunicación y la experiencia como fundamentales para el éxito del tratamiento.
+                Programas de tratamiento diseñados para mejorar la calidad de
+                vida y la participación de personas con discapacidad en diversos
+                ámbitos. En los consultorios, se enfocan en trabajar de manera
+                integral con profesionales y familias para lograr la autonomía
+                de cada paciente, estableciendo objetivos específicos y
+                enfatizando la comunicación y la experiencia como fundamentales
+                para el éxito del tratamiento.
               </p>
             </div>
-
           </section>
 
           {/* Footer para la tercera sección */}
           <footer className="w-full py-5 bg-gray-800 mt-auto fade-in">
             <div className="flex justify-center space-x-8 flex-wrap">
               {/* © 2024 Fundación DownIsUp. Todos los derechos reservados. */}
-            <p className="text-white text-lg md:text-md text-center">“Sabemos que el potencial de las personas con síndrome de Down no tiene límites.”</p>
+              <p className="text-white text-lg md:text-md text-center">
+                “Sabemos que el potencial de las personas con síndrome de Down
+                no tiene límites.”
+              </p>
             </div>
           </footer>
 
           {/* Cuarta sección */}
-          <section className="h-screen w-full flex flex-col md:flex-row snap-start transition-transform duration-1200 ease-in-out relative fade-in">
+
+          <section className="min-h-screen w-full flex flex-col md:flex-row snap-start transition-transform duration-1200 ease-in-out relative fade-in">
             <div className="w-full md:w-4/12 flex flex-col justify-center items-center lg:-mt-30 md:mt-0 p-6 md:p-10 z-10 bg-custom-blue md:bg-transparent">
               <div className="flex flex-col items-center md:items-start">
                 <p className="text-white text-xl md:text-2xl text-center font-bold leading-relaxed">
                   INFORMACIÓN DE CONTACTO
                 </p>
                 <p className="text-white text-lg mt-5 mb-5 leading-relaxed flex items-center">
-                  <PhoneIconSvg /> <a href="tel:+3517960194" className="text-white text-lg hover:underline">3517960194</a>
+                  <PhoneIconSvg />{" "}
+                  <a
+                    href="tel:+3517960194"
+                    className="text-white text-lg hover:underline"
+                  >
+                    3517960194
+                  </a>
                 </p>
                 <p className="text-white text-lg mt-5 mb-5 leading-relaxed flex items-center">
-                  <MessageIconSvg /><a href="mailto:fundaciondownisupcbacba@gmail.com" className="text-white text-lg hover:underline">fundaciondownisupcbacba@gmail.com</a>
+                  <MessageIconSvg />
+                  <a
+                    href="mailto:fundaciondownisupcbacba@gmail.com"
+                    className="text-white text-lg hover:underline"
+                  >
+                    fundaciondownisupcbacba@gmail.com
+                  </a>
                 </p>
-                <a href="https://maps.app.goo.gl/cpvhnUq2XH37BnCi8" className="text-white text-lg mt-5 mb-5 leading-relaxed flex items-center hover:underline"><PinIconSvg />Pérez de Herrera 2053</a>
+                <a
+                  href="https://maps.app.goo.gl/cpvhnUq2XH37BnCi8"
+                  className="text-white text-lg mt-5 mb-5 leading-relaxed flex items-center hover:underline"
+                >
+                  <PinIconSvg />
+                  Pérez de Herrera 2053
+                </a>
               </div>
             </div>
 
             {/* División diagonal, solo visible en pantallas medianas y más grandes */}
-            <div className="hidden md:block absolute w-full h-full -inset-x-0 z-0 bg-custom-blue" style={{ clipPath: "polygon(0 0, 38% 0, 25% 100%, 0% 100%)" }}></div>
+            <div
+              className="hidden md:block absolute w-full h-full -inset-x-0 z-0 bg-custom-blue"
+              style={{ clipPath: "polygon(0 0, 38% 0, 25% 100%, 0% 100%)" }}
+            ></div>
 
-            <div className="w-full md:w-8/12 flex flex-col justify-center items-center p-6 md:p-10 z-10">
+            <div className="w-full md:w-8/12 flex flex-col justify-center items-center p-6 md:p-10 z-10 sm:mx-4 md:mx-0 relative min-h-screen pb-10">
               {eventCards.map((event, index) => (
                 <article
                   key={index}
@@ -237,7 +330,6 @@ export default function Home() {
                   <h2 className="text-xl mt-4 mb-4 text-custom-blue font-semibold text-center">
                     {event.title}
                   </h2>
-
                   <div className="flex flex-col md:flex-row gap-x-4">
                     <Image
                       src={event.image}
@@ -255,7 +347,6 @@ export default function Home() {
                       </Button>
                     </div>
                   </div>
-
                   <div className="flex items-center gap-x-2 mt-4">
                     <CalendarIcon className="text-sm size-5 text-black" />
                     <h3 className="text-sm">{event.date}</h3>
@@ -264,16 +355,11 @@ export default function Home() {
               ))}
             </div>
           </section>
-
-
           {/* Footer para la cuarta sección */}
           <footer className="hidden md:block w-full mt-auto fade-in">
             <FooterWithLinks />
           </footer>
-
         </main>
-
-
       </div>
     </LandingLayout>
   );
